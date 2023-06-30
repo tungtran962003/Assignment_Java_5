da7698b02c37040b28f891e6ceca90e05cb6e926
tungttph27337 <tungtran962003@gmail.com>
2023-06-30 17:06:18 +0700

    first commit

---
+++ src/main/java/com/example/tungtt_ph27337_sof3021_assignment_java_5/TungTtPh27337Sof3021AssignmentJava5Application.java
@@ -0,0 +1,13 @@
+package com.example.tungtt_ph27337_sof3021_assignment_java_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TungTtPh27337Sof3021AssignmentJava5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TungTtPh27337Sof3021AssignmentJava5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tungtt_ph27337_sof3021_assignment_java_5/TungTtPh27337Sof3021AssignmentJava5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tungtt_ph27337_sof3021_assignment_java_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TungTtPh27337Sof3021AssignmentJava5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
